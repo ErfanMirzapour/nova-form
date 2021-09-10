589e5d2cb45d8e2389d255a15d2814001c8c213e
Erfan Mirzapour <52346515+ErfanMirzapour@users.noreply.github.com>
2021-09-10 15:47:02 +0430

    style: format all files

--- blitz.config.ts
+++ blitz.config.ts
@@ -1,13 +1,13 @@
-import { BlitzConfig, sessionMiddleware, simpleRolesIsAuthorized } from "blitz"
+import { BlitzConfig, sessionMiddleware, simpleRolesIsAuthorized } from 'blitz';
 
 const config: BlitzConfig = {
-  middleware: [
-    sessionMiddleware({
-      cookiePrefix: "blitz",
-      isAuthorized: simpleRolesIsAuthorized,
-    }),
-  ],
-  /* Uncomment this to customize the webpack config
+   middleware: [
+      sessionMiddleware({
+         cookiePrefix: 'blitz',
+         isAuthorized: simpleRolesIsAuthorized,
+      }),
+   ],
+   /* Uncomment this to customize the webpack config
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Note: we provide webpack above so you should not `require` it
     // Perform customizations to webpack config
@@ -15,5 +15,5 @@ const config: BlitzConfig = {
     return config
   },
   */
-}
-module.exports = config
+};
+module.exports = config;
