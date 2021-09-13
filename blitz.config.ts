5f99949e560d6ca5db5f54cc245d74b00815ab81
Erfan Mirzapour <52346515+ErfanMirzapour@users.noreply.github.com>
2021-09-14 00:16:48 +0430

    build: cancel type-checking and linting during before build

--- blitz.config.ts
+++ blitz.config.ts
@@ -7,6 +7,12 @@ const config: BlitzConfig = {
          isAuthorized: simpleRolesIsAuthorized,
       }),
    ],
+   eslint: {
+      ignoreDuringBuilds: true,
+   },
+   typescript: {
+      ignoreBuildErrors: true,
+   },
    /* Uncomment this to customize the webpack config
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Note: we provide webpack above so you should not `require` it
