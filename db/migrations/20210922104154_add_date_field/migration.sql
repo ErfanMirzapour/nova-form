0fa7b6dbcd85827fe9d9930e8da27a42ea21690d
Erfan Mirzapour <52346515+ErfanMirzapour@users.noreply.github.com>
2021-09-22 14:14:09 +0330

    feat: add createdAt and updatedAt fields

---
+++ db/migrations/20210922104154_add_date_field/migration.sql
@@ -0,0 +1,15 @@
+/*
+  Warnings:
+
+  - Added the required column `updatedAt` to the `Form` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Form" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "FormResult" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
