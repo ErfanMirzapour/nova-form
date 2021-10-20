b6b9645c6120210010b169a1338863a31b39bd04
Erfan Mirzapour <52346515+ErfanMirzapour@users.noreply.github.com>
2021-10-20 17:54:57 +0330

    fix: font urls

--- app/core/components/Fonts.tsx
+++ app/core/components/Fonts.tsx
@@ -7,37 +7,37 @@ const Fonts = () => (
         font-family: 'Iran Yekan';
         font-weight: 100;
         font-style-normal;
-        src: url('fonts/iranyekanwebthinfanum.woff') format('woff');
+        src: url('/fonts/iranyekanwebthinfanum.woff') format('woff');
       }
       @font-face {
         font-family: 'Iran Yekan';
         font-weight: 300;
         font-style-normal;
-        src: url('fonts/iranyekanweblightfanum.woff') format('woff');
+        src: url('/fonts/iranyekanweblightfanum.woff') format('woff');
       }
       @font-face {
         font-family: 'Iran Yekan';
         font-weight: 400;
         font-style-normal;
-        src: url('fonts/iranyekanwebregularfanum.woff') format('woff');
+        src: url('/fonts/iranyekanwebregularfanum.woff') format('woff');
       }
       @font-face {
         font-family: 'Iran Yekan';
         font-weight: 500;
         font-style-normal;
-        src: url('fonts/iranyekanwebmediumfanum.woff') format('woff');
+        src: url('/fonts/iranyekanwebmediumfanum.woff') format('woff');
       }
       @font-face {
         font-family: 'Iran Yekan';
         font-weight: 700;
         font-style-normal;
-        src: url('fonts/iranyekanwebboldfanum.woff') format('woff');
+        src: url('/fonts/iranyekanwebboldfanum.woff') format('woff');
       }
       @font-face {
         font-family: 'Iran Yekan';
         font-weight: 800;
         font-style-normal;
-        src: url('fonts/iranyekanwebextraboldfanum.woff') format('woff');
+        src: url('/fonts/iranyekanwebextraboldfanum.woff') format('woff');
       }
       `}
    />
